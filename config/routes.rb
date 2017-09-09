5df8fc8f74ef29ddf2c7e11b0c1a67cab573368c
hiro <zgmshioki@gmail.com>
2017-09-09 14:07:49 +0900

    Finished_RubyonRails4

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
   get 'posts/:id' => 'posts#show'
   get "/"=>"home#top"
   get "about"=>"home#about"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
