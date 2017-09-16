d35118ab719e5543dbd5af814632345d7ef492c2
hiro <zgmshioki@gmail.com>
2017-09-16 22:39:11 +0900

    Finished_RubyonRails678

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  
   def index
-    @posts=Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
   end
+  
   def show
     @post = Post.find_by(id: params[:id])
   end
+  
   def new
-    @post=Post.new
+    @post = Post.new
   end
+  
   def create
-    @post=Post.new(content: params[:content])
+    @post = Post.new(content: params[:content])
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
@@ -17,23 +22,27 @@ class PostsController < ApplicationController
       render("posts/new")
     end
   end
+  
   def edit
-    @post=Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
   end
+  
   def update
-    @post=Post.find_by(id: params[:id])
-    @post.content=params[:content]
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
     if @post.save
-      flash[:notice]="投稿を編集しました"
+      flash[:notice] = "投稿を編集しました"
       redirect_to("/posts/index")
     else
       render("posts/edit")
     end
   end
+  
   def destroy
-    @post=Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
+  
 end
